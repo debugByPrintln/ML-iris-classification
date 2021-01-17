44d0a32c3c874e6e9709a29111e2b8e68734a0e4
melnikov.n.89 <melnikov.n.89@mail.ru>
2021-01-18 01:07:59 +0300

    final

--- main.py
+++ main.py
@@ -14,7 +14,22 @@ XTrain, XTest, YTrain, YTest = train_test_split(irisDataset['data'], irisDataset
 
 irisDataframe = pd.DataFrame(XTrain, columns=irisDataset.feature_names)
 
-grr = pd.plotting.scatter_matrix(irisDataframe, c=YTrain, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
-plt.show()
+#grr = pd.plotting.scatter_matrix(irisDataframe, c=YTrain, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+#plt.show()
 
-print("Hello, GitHub!")
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+
+knn.fit(XTrain, YTrain)
+
+XNew = np.array([[7.2, 3.4, 6.1, 2.5]])
+
+prediction = knn.predict(XNew)
+print(format(prediction))
+print(irisDataset['target_names'][prediction])
+
+
+YPrediction = knn.predict(XTest)
+#print(YPrediction)
+
+print("accuracy =", + knn.score(XTest, YTest))
